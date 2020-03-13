75bc42382cb801ffe6c35b465619de4d631500a9
rcntype@gmail.com <rcntype@gmail.com>
2020-03-12 22:41:21 -0400

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')))
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')))
+
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const root = document.querySelector('#root')
+
+const App = () => {
+    return (
+        <hr/>
+    )
+}
+
+ReactDOM.render(<App/>, root)
